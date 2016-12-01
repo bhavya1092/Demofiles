4729fc8f7c2be590ee7d204177e75b3b4706daa7
bhavya1092 <sampatbhavya@yahoo.in>
2016-12-01 10:38:42 +0530

    Second commit

--- Abtractclass/src/p1/Area.java
+++ Abtractclass/src/p1/Area.java
@@ -1,9 +1,19 @@
 package p1;
 
-public class Area {
+public class Area 
+{
+	public void showArea()
+	{
+	  int a;
+	  
+            System.out.println("a");
+	  }
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Area a1=new Area();
+		a1.showArea();
+		
 
 	}
 
